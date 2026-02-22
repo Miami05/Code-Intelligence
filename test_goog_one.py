5f5251b6c7446325594a413279517b3d566eac05
Miami05 <lediodurmishaj02@gmail.com>
2026-02-22 16:40:54 +0100

    Adding test

---
+++ test_goog_one.py
@@ -0,0 +1,26 @@
+def add_numbers(a: int, b: int) -> int:
+    """
+    Add two numbers together.
+
+    Args:
+        a: First number
+        b: Second number
+
+    Returns:
+        Sum of a and b
+    """
+    return a + b
+
+
+def multiply_numbers(a: int, b: int) -> int:
+    """
+    Multiply two numbers together.
+
+    Args:
+        a: First number
+        b: Second number
+
+    Returns:
+        Product of a and b
+    """
+    return a * b
